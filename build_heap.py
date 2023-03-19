578a6096f98bd04a8b103cb41c2da8d55d78d33c
Ingus-Vanins <125833785+Ingus-Vanins@users.noreply.github.com>
2023-03-20 00:13:47 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -2,6 +2,7 @@
 
 
 def build_heap(data):
+    
     swaps = []
     n = len(data)
 
@@ -11,17 +12,19 @@ def build_heap(data):
     return swaps
 
 def mHeap(data, n, i, swaps):
+    
     l= 2 * i + 1
     r= 2 * i + 2
     min=i
 
-    if l <= n - 1 and data[l]<data[min]:
+    if l<=n - 1 and data[l]<data[min]:
         min=l
         
-    if r <= n - 1 and data[r]<data[min]:
+    if r<=n - 1 and data[r]<data[min]:
         min=r
         
     if i != min:
+        
         data[i], data[min] = data[min], data[i]
         swaps.append((i, min))
         mHeap(data, n, min, swaps)
@@ -33,17 +36,18 @@ def main():
     in1=input()
     
     if "I" in in1:
-        n = int(input())
-        data = list(map(int, input().split()))
+        n=int(input())
+        data=list(map(int, input().split()))
         
     elif "F" in in1:
+        
         in2=input()
         
         if "a" not in in2:
             
             with open("./tests/" + in2, mode='r') as fails:
-                n = int(fails.readline())
-                data = list(map(int,fails.readline().split()))
+                n=int(fails.readline())
+                data=list(map(int,fails.readline().split()))
                 
     else:
         print("error")
